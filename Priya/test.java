fd98dd55e6f8681eb2257d3a894cae1f105b7c66
priya <pg0776281@gmail.com>
2024-08-05 23:13:47 +0530

    Update test.java
    ....

--- Priya/test.java
+++ Priya/test.java
@@ -15,3 +15,152 @@ In object-oriented programming, the program is divided into small parts called o
   Object-oriented programming follows a bottom-up approach.
 
 */
+
+/*
+QUESTION-2
+
+In Java, classes and objects are basic concepts of Object Oriented Programming (OOPs)
+ that are used to represent real-world  entities. 
+ The class represents a group of objects having similar properties and behavior. For example, 
+the animal type Dog is a class while a particular dog named Tommy is an object of the Dog class.
+
+class is the type of blueprint 
+if does not acquire any memory space.
+
+while the object acquire memory space in the system.
+
+//Program for class
+
+class Student {
+    
+    int id;
+    String name;
+
+    public static void main(String args[])
+    {
+        
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
+
+ */
+
+
+ /*
+ QUESTION-3
+
+  There are four main principles of object-oriented programming. 
+  
+  Encapsulation
+  Abstraction
+  Inheritance 
+  Polymorphism
+
+  1// ENCAPSULATION--
+
+
+  It is used to hide the complete mechanism of how the implementation is taking place.
+   It helps to secure our data by protecting it from direct access by unauthorised entities.
+   It uses access specifiers to protect the internal state of our objects from outside entities. 
+
+   There are three major types of access specifiers which help to control the access of data.
+    These are public, private and protected specifiers.
+
+
+  2// ABSTRACTION-- 
+Abstraction is used to simplify the complex working of a class or method by hiding unnecessary details
+ and only highlighting the important details at the forefront.
+ It helps users to interact with the system properly without worrying about how everything is working here.
+
+
+ 3// INHERITENCE--
+ Inheritance is one of the most important concepts in object oriented programming. 
+ It helps us to use the features and properties of an existing class in another class. 
+ The child class can use the features of the parent class. It promotes code re-usability.
+
+ ** FOR INHERETING THE PROPERTIES OF SUPER CLASS IN SUB CLASS WE WILL USE THE " extends " KEYWORD.
+
+
+ 4// POLYMORPHISM--
+ Polymorphism in oops is one of the most important concepts in Object-oriented programming. 
+ It is a method in OOPS that tells that a class or entity occurs in different forms. 
+ ‘Poly’ means many, and ‘morphism’ means forms. Hence, polymorphism means to exist in many forms.
+
+ There are two types of Polymorphism: 
+ 1--Compile-time polymorphism 
+ 2--Runtime polymorphism. 
+
+>>>Compile Time Polymorphism: 
+The compile time polymorphism in Oops is achieved by using method overloading.
+ When more than one function has the same name but different parameters, it is called method overloading.
+
+>>>Runtime polymorphism: 
+Runtime polymorphism uses method overriding to achieve polymorphism.
+ The method call here is used at the runtime.
+  The method overriding is used in the subclass is already defined in
+   its superclass which is executed at the runtime. 
+  for rxample;;
+ A class Animal has a method sound () which can be called either for dog or a cat depending upon the call. 
+
+
+  */
+
+  /*
+  QUESTION-4
+
+  INHERITENSE IN JAVA:
+  Inheritance is one of the most important concepts in object oriented programming. 
+ It helps us to use the features and properties of an existing class in another class. 
+ The child class can use the features of the parent class. It promotes code re-usability.
+
+NEED OF INHERITENSE--
+>> IT PROMOTES CODE REUSABILITY.
+>> CAN INHERIT OR REUSE THE PROPERTIES OF SUPERCLASS IN SUBCLASS.
+
+   */
+
+
+   /*
+    QUESTION-5
+
+    ENCAPSULATION-
+
+  It is used to hide the complete mechanism of how the implementation is taking place.
+   It helps to secure our data by protecting it from direct access by unauthorised entities.
+   It uses access specifiers to protect the internal state of our objects from outside entities. 
+
+   There are three major types of access specifiers which help to control the access of data.
+    These are public, private and protected specifiers.
+
+
+    */
+
+    /*
+    QUESTION-6
+     
+ POLYMORPHISM--
+ Polymorphism in oops is one of the most important concepts in Object-oriented programming. 
+ It is a method in OOPS that tells that a class or entity occurs in different forms. 
+ ‘Poly’ means many, and ‘morphism’ means forms. Hence, polymorphism means to exist in many forms.
+
+ There are two types of Polymorphism: 
+ 1--Compile-time polymorphism 
+ 2--Runtime polymorphism. 
+
+>>>Compile Time Polymorphism: 
+The compile time polymorphism in Oops is achieved by using method overloading.
+ When more than one function has the same name but different parameters, it is called method overloading.
+
+>>>Runtime polymorphism: 
+Runtime polymorphism uses method overriding to achieve polymorphism.
+ The method call here is used at the runtime.
+  The method overriding is used in the subclass is already defined in
+   its superclass which is executed at the runtime. 
+  for rxample;;
+ A class Animal has a method sound () which can be called either for dog or a cat depending upon the call. 
+
+
+
+     */
