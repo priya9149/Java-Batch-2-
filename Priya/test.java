f46829687e6f9c08dad3ed475c456e0992722787
priya <pg0776281@gmail.com>
2024-08-05 22:38:30 +0530

    Create test.java
    PRIYA

---
+++ Priya/test.java
@@ -0,0 +1,17 @@
+/*  QUESTION 1---
+Object-oriented programming System(OOPs) is a programming concept that is based on “objects”.
+OOPs is a programming approach or a technique in which certain concepts are given to create a program in any certain language.
+ Any language that follows or implements the concepts of OOPs can be called Object Oriented Programing Language.
+OOPs concepts are just a description or a theory to create a program of any programming language.
+
+P= Polymorphism
+A=Abstraction
+I=Inharitance
+E=Encapsulation
+
+In procedural programming, the program is divided into small parts called functions. 
+In object-oriented programming, the program is divided into small parts called objects.
+ Procedural programming follows a top-down approach.
+  Object-oriented programming follows a bottom-up approach.
+
+*/
